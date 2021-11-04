5b71f8f0c304ef60c2d7adaf96a0b2121678ed08
Rakibul Hassan Rony <2018000000012@seu.edu.bd>
2021-11-04 23:08:02 +0600

    product-list UI integration, and Backend API and CORS,

--- ecommerce-backend/src/main/java/com/rony/ecommerce/dao/ProductCategoryRepository.java
+++ ecommerce-backend/src/main/java/com/rony/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,9 @@ import com.rony.ecommerce.entity.Product;
 import com.rony.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 
