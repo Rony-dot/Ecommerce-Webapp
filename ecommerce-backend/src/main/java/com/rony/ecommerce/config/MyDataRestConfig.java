f152905e7ee08bb1aaeb126306c5c771a96942f3
Rakibul Hassan Rony <2018000000012@seu.edu.bd>
2021-11-05 23:41:42 +0600

    find by product-category

--- ecommerce-backend/src/main/java/com/rony/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-backend/src/main/java/com/rony/ecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,25 @@ package com.rony.ecommerce.config;
 
 import com.rony.ecommerce.entity.Product;
 import com.rony.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Autowired
+    private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] theUnsupportedMethods = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
@@ -23,6 +34,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedMethods))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedMethods)));
 
+        exposeIds(config);
+    }
 
+    private void exposeIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for(EntityType entityType: entities){
+            entityClasses.add(entityType.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
