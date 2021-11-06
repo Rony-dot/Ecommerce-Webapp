2fba3d18ed9b37f41024f7df709275a4d24b5a3e
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2021-11-06 22:05:56 +0600

    minor 1

--- ecommerce-backend/src/main/java/com/rony/ecommerce/dao/ProductCategoryRepository.java
+++ ecommerce-backend/src/main/java/com/rony/ecommerce/dao/ProductCategoryRepository.java
@@ -9,5 +9,4 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 
-
 }
